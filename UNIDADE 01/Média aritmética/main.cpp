6fbd1a40dd426e510b7de64c9410461c27653089
Rychardson Ribeiro de Souza <93292522+RychardRS@users.noreply.github.com>
2022-06-27 12:59:11 -0300

    Create main.cpp

---
+++ UNIDADE 01/Média aritmética/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*Media Escreva um programa que lê três números inteiros e imprime a média aritmética deles.*/
+
+    int n1, n2, n3;
+    cin>>n1;
+    cin>>n2;
+    cin>>n3;
+    float media, x;
+    x=(n1+n2+n3);
+    media=x/3;
+
+    cout<<media<<endl;
+    return 0;
+}
